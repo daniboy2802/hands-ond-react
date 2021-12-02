42a972789a812dcc6c0fa35e725b6f132656c897
Daniboy2802 <dv.santibanez@gmail.com>
2021-12-02 08:30:25 -0600

    initial set up of the dashboard is ready

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,21 +1,18 @@
 import Styles from '../styles/dashboard.module.css'
+import useGetHeroes from '../useGetHeroes'
 import { Link } from 'react-router-dom'
 
 export default function Dashboard() {
+  const heroes = useGetHeroes()
+  const superHeroes = heroes.filter(heroe => heroe.id > 15)
+  
   return (
     <div className={Styles['container']}>
-      <Link  to='/'>
-        <div className={Styles['element']}>hola pai</div>
-      </Link>
-      <Link to='/'>
-        <div className={Styles['element']}>hola pai</div>
-      </Link>
-      <Link to='/'>
-        <div className={Styles['element']}>hola pai</div>
-      </Link>
-      <Link to='/'>
-        <div className={Styles['element']}>hola pai</div>
-      </Link>
+      {superHeroes.map(heroe => (
+        <Link to={`/edit/${heroe.id}`} className={Styles['link']}>
+          <div className={Styles['element']}><p>{heroe.name}</p></div>
+        </Link>
+      ))}
     </div>
   )
 }
--- src/useGetHeroes.js
+++ src/useGetHeroes.js
@@ -2,10 +2,11 @@ import { useState, useEffect } from 'react';
 
 export default function useGetHeroes() {
   const [heroes, setHeroes] = useState([])
+  
   useEffect(() => {
     let heroesTest = []
     const ids = Object.keys(window.localStorage)
-    // Fixed the production problem repeats the element after every reload
+  
     ids.forEach(id => {
       let name = window.localStorage.getItem(id)
       let heroe = {
